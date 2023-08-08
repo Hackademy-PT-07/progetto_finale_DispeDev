6c075b04708940373a43fe490a03223dde26ee0b
elisa digiacomo <elisa95d@hotmail.it>
2023-08-08 20:02:00 +0200

    modifiche register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,19 @@
 <x-main>
     <x-slot:mainTitle>
-        Homepage
+        Registrati
         </x-slot>
+        <div class="container">
+            <div class="row">
+        <h1 class="text-center my-5">Registrati</h1>
+        <div class="col-6">
+            <img class="img-fluid rounded"src="https://www.ofhousesandtrees.com/wp-content/uploads/2021/05/How-to-Print-a-Pinterest-Board-1-683x1024.png" alt="">
+        </div>
+
+        <div class="col-6  text-center" method="POST" route="{{route('register')}}">
         <form>
-            <div class="container">
+              @csrf
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                    <label for="exampleInputEmail1" class="form-label">Nome</label>
                     <input type="text" name="name" class="form-control" id="exampleInputEmail1"
                         aria-describedby="nameHelp">
                     @error('name')
@@ -13,7 +21,7 @@
                     @enderror
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <label for="exampleInputEmail1" class="form-label">Indirizzo e-mail</label>
                     <input type="email" name="email" class="form-control" id="exampleInputEmail1"
                         aria-describedby="emailHelp">
                     @error('email')
@@ -35,7 +43,9 @@
                         {{ $message }}
                     @enderror
                 </div>
-                <button type="submit" class="btn btn-primary">Register</button>
+                <button type="submit" class="btn btn-azzuro">Registrati</button>
+            </div>
+            </div>
             </div>
         </form>
 </x-main>
