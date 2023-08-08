030e4eb26e209a104da0cf836e4c5856efca41aa
Thomas Armao <thomas.armao.93@gmail.com>
2023-08-08 19:24:39 +0200

    Aggiungere il percorso del logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-arancio">
     <div class="container-fluid">
-        <a class="navbar-brand fc-bianco fs-2 fw-bold" href="#"><img src=".public/storage/img/logo_presto.it_bianco.png" alt=""></a>
+        <a class="navbar-brand fc-bianco fs-2 fw-bold" href="#"><img src="" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
