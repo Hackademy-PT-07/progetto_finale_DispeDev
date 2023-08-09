020cd556fbdfea895de0c3f1a6ca416244dcbe43
antonio annunziata <antonio.annunziata@aol.com>
2023-08-09 10:34:05 +0200

    modifiche auth navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,44 +1,40 @@
 <nav class="navbar navbar-expand-lg bg-body-arancio">
     <div class="container-fluid">
         <a class="navbar-brand fc-bianco fs-2 fw-bold" href="#"><img src="/resources/css/logo_presto.it" alt=""></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav fc-bianco">
                 @if (auth()->user())
-                    <li class="nav-item">
-                        <a class="nav-link text-white" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Features</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false" >
-                            {{auth()->user()->name}}
-                        </a>
-                        <ul class="dropdown-menu text-white">
-                            <li><a class="dropdown-item" href="#"></a></li>
-                            <li><a class="dropdown-item" href="#"></a></li>
-                            <li><a class="dropdown-item" href="#"></a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <form action="/logout" method="POST">
-                            @csrf
-                        <button class="btn text-white" type="submit">Esci</button> 
-                        </form>
-                        
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" aria-current="page" href="{{route('homepage')}}">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{auth()->user()->name}}
+                    </a>
+                    <ul class="dropdown-menu text-white">
+                        <li><a class="dropdown-item" href="#">Diventa un revisore</a></li>
+                        <li><a class="dropdown-item" href="#">Ciao</a></li>
+                        <li><a class="dropdown-item" href="#">Ciao</a></li>
+                        <li class="nav-item">
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <button class="btn text-black" type="submit">Esci</button>
+                            </form>
+
+                        </li>
+                    </ul>
+                </li>
+
                 @else
-                    <li class="nav-item">
-                        <a class="nav-link fc-bianco" aria-current="page" href="/login">Accedi</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link fc-bianco" aria-current="page" href="/register">Registrati</a>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link fc-bianco" aria-current="page" href="/login">Accedi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fc-bianco" aria-current="page" href="/register">Registrati</a>
+                </li>
                 @endif
             </ul>
         </div>
