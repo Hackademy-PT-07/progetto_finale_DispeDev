32b53bd81cd2004987eaf07bd8b47da8ec25a58a
elisa digiacomo <elisa95d@hotmail.it>
2023-08-10 19:04:57 +0200

    fix errors

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -10,6 +10,7 @@ class HomepageController extends Controller
     public function homepage()
     {
         $announcements = Announcement::take(6)->get()->sortByDesc('created_at');
+       
 
         return view('homepage', compact('announcements'));
     }
