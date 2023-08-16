3cc55bb851a48311d3d84bd394b55b9ad85e61f9
Thomas Armao <thomas.armao.93@gmail.com>
2023-08-16 16:03:22 +0200

    search bar responsive

--- resources/views/components/search_bar.blade.php
+++ resources/views/components/search_bar.blade.php
@@ -7,19 +7,19 @@
                 <div class="search-input">
                     <label for="">In quale categoria?</label>
                     <div class="select-categories">
-                    <select name="" id="">
-                        <option value="" disabled selected>Segli una categoria</option>
-                        <option value="Auto">Auto</option>
-                        <option value="Moto">Moto</option>
-                        <option value="Nautica">Nautica</option>
-                        <option value="Elettronica">Elettronica</option>
-                        <option value="Casalinghi">Casalinghi</option>
-                        <option value="Arredamento">Arredamento</option>
-                        <option value="Giardinaggio">Giardinaggio</option>
-                        <option value="Animali">Animali</option>
-                        <option value="Immobili">Immobili</option>
-                        <option value="Lavoro">Lavoro</option>
-                    </select>
+                        <select name="" id="">
+                            <option value="" disabled selected>Segli una categoria</option>
+                            <option value="Auto">Auto</option>
+                            <option value="Moto">Moto</option>
+                            <option value="Nautica">Nautica</option>
+                            <option value="Elettronica">Elettronica</option>
+                            <option value="Casalinghi">Casalinghi</option>
+                            <option value="Arredamento">Arredamento</option>
+                            <option value="Giardinaggio">Giardinaggio</option>
+                            <option value="Animali">Animali</option>
+                            <option value="Immobili">Immobili</option>
+                            <option value="Lavoro">Lavoro</option>
+                        </select>
                 </div>
                 </div>
                 <div class="search-input">
