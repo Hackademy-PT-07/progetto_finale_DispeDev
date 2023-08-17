356fe96a704ca20e008a06af64815ede72dabff7
Thomas Armao <thomas.armao.93@gmail.com>
2023-08-17 22:59:52 +0200

    correzione card annunci

--- resources/views/components/card-announcement.blade.php
+++ resources/views/components/card-announcement.blade.php
@@ -14,7 +14,7 @@
 </div> --}} -->
 
 
-<div class="mt-5 mx-auto card card-radius mb-3" style="max-width: 600px;">
+<!-- <div class="mt-5 mx-auto card card-radius mb-3" style="max-width: 600px;">
     <div class="row g-0">
         <div class="col-md-6">
             <img src="{{ $url_image }}" class="img-fluid full-size" alt="{{ $title }}">
@@ -29,4 +29,19 @@
             </div>
         </div>
     </div>
+</div> -->
+
+
+<div class="card-announcement">
+        <div class="card-img-box">
+            <img class="card-announcement-img" src="https://picsum.photos/200/220" alt="">
+        </div>
+        <div class="card-announcement-text-box">
+            <h3>Titolo</h3>
+            <span>€ 0,00</span>
+            <p>Breve descrizione del prodotto... Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem laboriosam itaque beatae sit, labore odit sunt optio fuga corrupti officia, sint laudantium iste at eius, provident cumque ratione molestiae animi.</p>
+            <a href="" class="category-a">Categoria</a>
+        </div>
+        
+
 </div>
