eb583f7389b479c960b2e1397dbaf639bea5c59f
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2023-08-22 18:34:50 +0200

    fix 2.4

--- resources/views/components/search_bar.blade.php
+++ resources/views/components/search_bar.blade.php
@@ -1,24 +1,24 @@
 <div class="search-box">
-            <form action="{{route('annunci.categoria')}}" method="POST">
-                @csrf
-                <div class="search-input">
-                    <label for="">Cosa cerchi?</label>
-                    <input type="text" placeholder="Cerca">
-                </div>
-                <div class="search-input">
-                    <label for="">In quale categoria?</label>
-                    <div class="select-categories">
-                        <select name="category_id" id="category_id">
-                            <option selected>Segli una categoria</option>
-                            @foreach($categories as $category)
-                            <option value="{{$category->id}}">{{$category->name}}</option>
-                            @endforeach
-                            
-                        </select>
-                </div>
-                </div>
-                <div class="search-input">
-                    <button class="search-button"><i class="bi bi-search"></i></button>
-                </div>
-            </form>
+    <form action="{{ route('annunci.categoria') }}" method="POST">
+        @csrf
+        <div class="search-input">
+            <label for="">Cosa cerchi?</label>
+            <input type="text" placeholder="Cerca">
         </div>
+        <div class="search-input">
+            <label for="">In quale categoria?</label>
+            <div class="select-categories">
+                <select name="category_id" id="category_id">
+                    <option selected>Segli una categoria</option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+
+                </select>
+            </div>
+        </div>
+        <div class="search-input">
+            <button class="search-button"><i class="bi bi-search"></i></button>
+        </div>
+    </form>
+</div>
