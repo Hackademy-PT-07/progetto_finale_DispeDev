741872ed7e041c998bdaf712d939fa8a668e0292
antonio annunziata <antonio.annunziata@aol.com>
2023-08-23 18:30:13 +0200

    new livewire component user-announcements-list

---
+++ resources/views/livewire/user-announcements-list.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <div class="card bg-body-panna my-4">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h6 class="text-body-tertiary mb-0">categoria</h6>
+            <span class="text-body-tertiary">12/05/20</span>
+        </div>
+        <div class="card-body d-flex justify-content-between">
+            <div class="text-container">
+                <h5 class="card-title">TITOLO ANNUNCIO</h5>
+                <p class="card-text small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+                    excepturi, fuga maiores autem id recusandae quos repellendus natus, ullam, ea eaque
+                    delectus. Dolores aut id consectetur quis, accusantium aperiam? Ea!</p>
+            </div>
+            <div class="btn-container">
+                <button class="btn btn-warning text-white bottom"><i class="bi bi-pencil-fill"></i></button>
+                <button class="btn btn-danger"><i class="bi bi-trash-fill"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
