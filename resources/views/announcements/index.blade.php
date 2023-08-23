4048e3449ab119df63e65486e495ed097b3e1aba
Thomas Armao <thomas.armao.93@gmail.com>
2023-08-23 21:13:51 +0200

    sistemata pagina index degli annunci

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -1,39 +1,7 @@
 <x-main>
-    <x-slot:mainTitle>
-        Annunci
-        </x-slot>
-
+    <x-slot:mainTitle>Annunci</x-slot>
         <div class="container">
-            <form action="{{route('annunci.categoria')}}" method="POST" class="d-flex justify-content-between">
-                @csrf
-                <div>
-                    <label for="keyword-search" id="keyword-search">Cosa cerchi?</label>
-                    <div class="d-flex">
-                        <i class="bi bi-search"></i>
-                    </div>
-                    <input type="search" id="keyword-search" name="keyword-search" placeholder="Vespa, iPhone, Bilocale" autocomplete="off" value="">
-                </div>
-
-                <div>
-                    <label for="category_id">Scegli una categoria</label>
-                    <div class="d-flex">
-                        <i class="bi bi-archive"></i>
-                    </div>
-                    <select name="category_id" id="category_id">
-                        <option selected>Seleziona</option>
-                        @foreach($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-
-                <div>
-                    <button type="submit" class="btn btn-danger">
-                        <i class="bi bi-search"></i>
-                    </button>
-                </div>
-            </form>
-
+            <x-search_bar />
             <h1>Annunci</h1>
             <p>{{$announcements->count()}} Annunci trovati</p>
             <div class="container">
