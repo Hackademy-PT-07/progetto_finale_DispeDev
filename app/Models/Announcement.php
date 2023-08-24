0d310033936089aa99687675ce45cdd5f9b8b27f
antonio annunziata <antonio.annunziata@aol.com>
2023-08-24 18:30:37 +0200

    installazione tnt search

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -4,11 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Announcement extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
+    public function toSearchableArray() 
+    {
+        $category = $this->category;
+        $array = [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'category' => $category,
+        ];
+
+        return $array;
+    }
+    
     protected $fillable = ['title', 'description', 'category_id', 'user_id', 'url_image', 'price'];
 
     public function category()
