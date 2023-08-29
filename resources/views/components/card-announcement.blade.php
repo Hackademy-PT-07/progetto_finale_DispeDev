cdb6e8fb60487a0297bae48df251574cac7d7d46
Thomas Armao <thomas.armao.93@gmail.com>
2023-08-29 21:19:46 +0200

    angoli di nuovo arrotondati e card annunci mobile modificata

--- resources/views/components/card-announcement.blade.php
+++ resources/views/components/card-announcement.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('annunci.show', $id) }}">
+<!-- <a href="{{ route('annunci.show', $id) }}"> -->
     <div class="card-announcement">
         <div class="card-img-box">
             <img class="card-announcement-img" src="{{ $url_image }}" alt="{{ $title }}">
@@ -8,9 +8,9 @@
             <h3>{{ $title }}</h3>
             <span>€{{ $price }}</span>
             <p>{{ $description }}</p>
-            <div class="d-flex justify-content-end mt-4">
+            <div class="d-flex justify-content-end">
                 <p class="last-update">Ultima modifica: {{ $updated }}</p>
             </div>
         </div>
     </div>
-</a>
+<!-- </a> -->
