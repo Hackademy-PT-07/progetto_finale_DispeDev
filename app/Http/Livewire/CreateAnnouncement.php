3248c53698a1f073978ca7cc8e712ab80c5faa0a
elisa digiacomo <elisa95d@hotmail.it>
2023-08-29 21:31:57 +0200

    file upload

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -8,6 +8,7 @@ use App\Models\Category;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Storage;
+use App\Jobs\ResizeImage;
 
 class CreateAnnouncement extends Component
 {
@@ -54,20 +55,25 @@ protected $listeners = [
 
     public function store(){
         
-        /*$this->validate([
-
-            'url_image' => 'image|max:1024', // 1MB Max
-
-        ]);*/
-    
-        $category = Category::find($this->category_id); /* mi trova la categoria con quel tipo di category id e da category mi crea l'annuncio */ 
-        $category->announcements()->create([
+        $this->validate();
+        $category = Category::find($this->category_id)->announcements->create($this->validate()); /* mi trova la categoria con quel tipo di category id e da category mi crea l'annuncio */ 
+        /*$category->announcements()->create([
             'title'=> $this->title,
             'description'=>$this->description,
             'price'=>$this->price,
             #'url_image'=>$this->url_image->store('photos'),
             'user_id'=> auth()->user()->id,
-        ]);
+        ]);*/
+        if(count($this->url_image)){
+            foreach($this->url_image as $image){
+                /*$this->announcement->images()->create(['path'=> $image->store('images', 'public')]);*/
+                $newFileName = "announcements/{$this->announcement->id}";
+                $newImage = $this->announcement->images()->create(['path'=> $image->store($newFileName, 'public')]);
+            
+                dispatch(new ResizeImage($newImage->path, 400, 300));
+            }
+            File::deleteDirectory(storage_path('/app/livewire-tmp'));
+        }
 
         $this->resetImputFields();
 
