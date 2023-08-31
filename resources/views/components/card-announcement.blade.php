a7d5f8c843911ee13c8c5e88adf1b1c38c87c2ba
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2023-08-31 21:24:09 +0200

    Homepage 2.0

--- resources/views/components/card-announcement.blade.php
+++ resources/views/components/card-announcement.blade.php
@@ -1,16 +1,18 @@
-<a class="a-card" href="{{ route('annunci.show', $id) }}">
-    <div class="card-announcement">
-        <div class="card-img-box">
-            <img class="card-announcement-img" src="{{ $url_image }}" alt="{{ $title }}">
-        </div>
-        <div class="card-announcement-text-box">
-            <a href="{{route('annunci.categoria', ['category' => $categoria])}}" class="category-a">{{ $category }}</a>
-            <h3>{{ $title }}</h3>
-            <span>€{{ $price }}</span>
-            <p>{{ $description }}</p>
-            <div class="d-flex justify-content-end">
-                <p class="last-update">Ultima modifica: {{ $updated }}</p>
-            </div>
+{{-- <a class="a-card" href="{{ route('annunci.show', $id) }}">
+
+</a> --}}
+
+<div class="card-announcement">
+    <div class="card-img-box">
+        <img class="card-announcement-img" src="{{ $url_image }}" alt="{{ $title }}">
+    </div>
+    <div class="card-announcement-text-box">
+        <a href="{{ route('annunci.categoria', ['category' => $categoria]) }}" class="category-a">{{ $category }}</a>
+        <h3>{{ $title }}</h3>
+        <span>€{{ $price }}</span>
+        <p>{{ $description }}</p>
+        <div class="d-flex justify-content-end">
+            <p class="last-update">Ultima modifica: {{ $updated }}</p>
         </div>
     </div>
-</a>
+</div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,73 +3,76 @@
         <a class="navbar-brand" href="/">
             <img class="img-logo" src="{{ asset('img/logo-presto-bianco.png') }}" alt="">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 @if (auth()->user())
-                <li class="nav-item">
-                    <x-lang lang="it" nation="it" />
-                </li>
-                <li class="nav-item">
-                    <x-lang lang="en" nation="gb" />
-                </li>
-                <li class="nav-item">
-                    <x-lang lang="ch" nation="cn" />
-                </li>
-                <div class="row mx-2">
-                    <button class="btn-add">
-                        <a href="{{ route('annuncio.create') }}"><i class="bi bi-plus-square pe-2"></i>{{__('ui.insertAnnouncement')}}</a>
-                    </button>
-                </div>
-
-                @if (Auth::user()->is_revisor)
-                <li class="nav-item revisor-icon">
-                    <a href="{{ route('revisor.index') }}" class="nav-text " aria-current="page">{{__('ui.revisorArea')}}
-                    </a>
-                    <span>{{ App\Models\Announcement::toBeRevisionedCount() }}</span>
-                </li>
-                @endif
-
-                <li class="nav-item dropdown nav-text">
-                    <a class="nav-link nav-text" href="{{ route('annunci.index') }}">
-                        {{__('ui.announcements')}}</a>
-                </li>
-                <li class="nav-link nav-text" href="#" role="button">
-                    {{ auth()->user()->name }}
-                </li>
-                <li class="nav-item dropdown nav-text">
-                    <form action="/logout" method="POST">
-                        @csrf
-                        <button class="btn nav-text" type="submit">{{__('ui.logout')}}</button>
-                    </form>
-                </li>
-
-                <ul class="dropdown-menu">
                     <li class="nav-item">
-                        <div>
-                            <a href="{{ route('i.tuoi.annunci') }}">{{__('ui.yourAnnouncements')}}</a>
-                        </div>
+                        <x-lang lang="it" nation="it" />
                     </li>
-                </ul>
+                    <li class="nav-item">
+                        <x-lang lang="en" nation="gb" />
+                    </li>
+                    <li class="nav-item">
+                        <x-lang lang="ch" nation="cn" />
+                    </li>
+                    <div class="row mx-2">
+                        <button class="btn-add">
+                            <a href="{{ route('annuncio.create') }}"><i
+                                    class="bi bi-plus-square pe-2"></i>{{ __('ui.insertAnnouncement') }}</a>
+                        </button>
+                    </div>
+
+                    @if (Auth::user()->is_revisor)
+                        <li class="nav-item revisor-icon">
+                            <a href="{{ route('revisor.index') }}" class="nav-text "
+                                aria-current="page">{{ __('ui.revisorArea') }}
+                            </a>
+                            <span>{{ App\Models\Announcement::toBeRevisionedCount() }}</span>
+                        </li>
+                    @endif
+
+                    <li class="nav-item dropdown nav-text">
+                        <a class="nav-link nav-text" href="{{ route('annunci.index') }}">
+                            {{ __('ui.announcements') }}</a>
+                    </li>
+                    <li class="nav-link nav-text" href="#" role="button">
+                        {{ auth()->user()->name }}
+                    </li>
+                    <li class="nav-item dropdown nav-text">
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <button class="btn nav-text" type="submit">{{ __('ui.logout') }}</button>
+                        </form>
+                    </li>
+
+                    <ul class="dropdown-menu">
+                        <li class="nav-item">
+                            <div>
+                                <a href="{{ route('i.tuoi.annunci') }}">{{ __('ui.yourAnnouncements') }}</a>
+                            </div>
+                        </li>
+                    </ul>
                 @else
-                <li class="nav-item">
-                    <x-lang lang="it" nation="it" />
-                </li>
-                <li class="nav-item">
-                    <x-lang lang="en" nation="gb" />
-                </li>
-                <li class="nav-item">
-                    <x-lang lang="ch" nation="cn" />
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link nav-text" aria-current="page" href="/login">{{__('ui.login')}}</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link nav-text" aria-current="page" href="/register">{{__('ui.register')}}</a>
-                </li>
+                    <li class="nav-item">
+                        <x-lang lang="it" nation="it" />
+                    </li>
+                    <li class="nav-item">
+                        <x-lang lang="en" nation="gb" />
+                    </li>
+                    <li class="nav-item">
+                        <x-lang lang="ch" nation="cn" />
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link nav-text" aria-current="page" href="/login">{{ __('ui.login') }}</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link nav-text" aria-current="page" href="/register">{{ __('ui.register') }}</a>
+                    </li>
                 @endif
             </ul>
         </div>
