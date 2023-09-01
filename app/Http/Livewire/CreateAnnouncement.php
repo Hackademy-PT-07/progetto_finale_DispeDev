634ee1651aff3eeeba35f063d346cfad78065f27
antonio annunziata <antonio.annunziata@aol.com>
2023-09-02 01:42:33 +0200

    completato Google apis

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
 use App\Models\User;
 use Livewire\Component;
@@ -114,6 +115,7 @@ protected $listeners = [
             
                 dispatch(new ResizeImage($newImage->path, 400, 300));
                 dispatch(new GoogleVisionSafeSearch($newImage->id));
+                dispatch(new GoogleVisionLabelImage($newImage->id));
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -15,66 +15,87 @@
                     <div class="carousel slide" id="carousel" data-bs-ride="carousel">
                         @if ($announcementToCheck->images)
                         <div class="carousel-inner">
-                        @foreach($announcementToCheck->images as $image)
+                            @foreach($announcementToCheck->images as $image)
                             <div class="carousel-item @if($loop->first)active @endif">
                                 <img src="{{$image->getUrl(400,300)}}" alt="foto">
-                                </div>
-                        
+                            </div>
+
                             @endforeach
-                            </div>
-                            @else
-                            <div class="carousel-item">
-                                <img class="img-fluid" src="https://picsum.photos/200/300" alt="foto2">
-                            </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="carousel"
-                            data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="carousel"
-                            data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-                    @endif
-                    <div class="content-container">
-                        <div class="content-item">
-                            {{__('ui.titlePlaceholder')}}:<h5 class="content-title"> {{ $announcementToCheck->title }}</h5>
-                        </div>
-
-
-                        <div class="content-item">
-                        {{__('ui.category')}}:<h5 class="content-category"> {{ $announcementToCheck->category->name }}</h5>
-                        </div>
-                        <div class="content-item">
-                        {{__('ui.description')}}:<h5 class="content-text"> {{ $announcementToCheck->description }}</h5>
-                        </div>
-                        <div class="content-item mb-4">
-                        {{__('ui.publishedAt')}}:<h5 class="content-footer">
-                                {{ $announcementToCheck->created_at->format('d/m/Y') }}</h5>
+                        @else
+                        <div class="carousel-item">
+                            <img class="img-fluid" src="https://picsum.photos/200/300" alt="foto2">
                         </div>
                     </div>
-
-                    <div class="check-container">
-                        <form action="{{ route('accept_announcement', ['announcement' => $announcementToCheck]) }}"
-                            method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <button type="submit" class="btn btn-success shadow">{{__('ui.accept')}}</button>
-                        </form>
-
-                        <form action="{{ route('reject_announcement', ['announcement' => $announcementToCheck]) }}"
-                            method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <button type="submit" class="btn btn-danger shadow">{{__('ui.reject')}}</button>
-                        </form>
-                    </div>
-
+                    <button class="carousel-control-prev" type="button" data-bs-target="carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
+                @endif
+                <div class="content-container">
+                    <div class="content-item">
+                        {{__('ui.titlePlaceholder')}}:<h5 class="content-title"> {{ $announcementToCheck->title }}</h5>
+                    </div>
+
+
+                    <div class="content-item">
+                        {{__('ui.category')}}:<h5 class="content-category"> {{ $announcementToCheck->category->name }}</h5>
+                    </div>
+                    <div class="content-item">
+                        {{__('ui.description')}}:<h5 class="content-text"> {{ $announcementToCheck->description }}</h5>
+                    </div>
+                    <div class="content-item mb-4">
+                        {{__('ui.publishedAt')}}:<h5 class="content-footer">
+                            {{ $announcementToCheck->created_at->format('d/m/Y') }}
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="check-container">
+                    <form action="{{ route('accept_announcement', ['announcement' => $announcementToCheck]) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <button type="submit" class="btn btn-success shadow">{{__('ui.accept')}}</button>
+                    </form>
+
+                    <form action="{{ route('reject_announcement', ['announcement' => $announcementToCheck]) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <button type="submit" class="btn btn-danger shadow">{{__('ui.reject')}}</button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+        </div>
+
+        <div>
+            <h5>Tags</h5>
+            <div>
+                @if($image->labels)
+                    @foreach($image->labels as $label)
+                        <p>{{$label}}</p>
+                    @endforeach
+                @endif
+            </div>
+        </div>
+        <div>
+            <div>
+                <h5>Revisione Immagini</h5>
+                    <p>Adulti: <span class="{{$image->adult}}"></span></p>
+                    <p>Satira: <span class="{{$image->spoon}}"></span></p>
+                    <p>Medicina: <span class="{{$image->medicine}}"></span></p>
+                    <p>Violenza: <span class="{{$image->violence}}"></span></p>
+                    <p>Contenuto Ammiccante: <span class="{{$image->racy}}"></span></p>
             </div>
         </div>
         @endif
+
+        
+
 </x-main>
