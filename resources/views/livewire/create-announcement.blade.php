345ed07ace91911c49d1e39d1cbd180f40a925ac
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2023-09-05 23:27:32 +0200

    Inserisci Annuncio

--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,60 +1,59 @@
-<div class="container my-auto">
-    <div class="row mt-announce">
+<div class="container {{-- mt-announce --}}">
+    <div class="row">
         <x-success />
-        <div class="col-4">
-            <div class="card-container">
-                @if ($announcement->id)
-                    <h3>{{ __('ui.editAnnouncement') }}</h3>
-                @else
-                    <h3>{{ __('ui.createAnnouncement') }}</h3>
-                @endif
-                <form wire:submit.prevent="store" enctype="multipart/form-data"
-                    class="d-flex flex-column justify-content-center">
-                    @csrf
-                    {{-- First Section --}}
-                    <div class="section-split mb-3">
-                        <input type="text" name="title" class="form-control" id="title" wire:model="title"
-                            value="{{ old('title') }}" placeholder="{{ __('ui.titlePlaceholder') }}">
-                        @error('title')
-                            <span class="small text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    {{-- Second Section --}}
-                    <div class="section-split mb-3">
-                        <input type="number" name="price" class="form-control" id="price" wire:model="price"
-                            value="{{ old('price') }}" placeholder="{{ __('ui.pricePlaceholder') }}">
-                        @error('price')
-                            <span class="small text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    {{-- Third Section --}}
-                    <div class="section-split mb-3">
-                        <select class="form-select" wire:model="category_id" name="category_id"
-                            aria-label="Default select example">
-                            <option selected>{{ __('ui.selectCategories') }}</option>
-                            @foreach ($categories as $category)
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endforeach
-                        </select>
-                        @error('category_id')
-                            <span class="small text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    {{-- Image Section --}}
-                    <div class="mb-3">
-                        <input wire:model="temporary_images" type="file" name="images" multiple
-                            class="form-control shadow @error('temporary_images.*') is-invalid @enderror"
-                            placeholder="Img" />
-                        @error('temporary_images.*')
-                            <p class="text-danger mt-2">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    {{-- Image empty Section --}}
-                    @if (!empty($images) || !empty($dbimages))
-                        <div class="section-split mb-3">
-                            <p>Photo preview:</p>
-                            <div class="row border border-4 border-info rounded shadow py-4">
-                                @if($images != null)
+        <div class="card-container">
+            @if ($announcement->id)
+                <h3 class="card-create--title">{{ __('ui.editAnnouncement') }}</h3>
+            @else
+                <h3 class="card-create--title">{{ __('ui.createAnnouncement') }}</h3>
+            @endif
+            <form wire:submit.prevent="store" enctype="multipart/form-data"
+                class="d-flex flex-column justify-content-center">
+                @csrf
+                {{-- First Section --}}
+                <div class="section-text mb-3">
+                    <input type="text" name="title" class="form-control" id="title" wire:model="title"
+                        value="{{ old('title') }}" placeholder="{{ __('ui.titlePlaceholder') }}">
+                    @error('title')
+                        <span class="small text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                {{-- Second Section --}}
+                <div class="section-text mb-3">
+                    <input type="number" name="price" class="form-control" id="price" wire:model="price"
+                        value="{{ old('price') }}" placeholder="{{ __('ui.pricePlaceholder') }}">
+                    @error('price')
+                        <span class="small text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                {{-- Third Section --}}
+                <div class="section-text mb-3">
+                    <select class="form-select" wire:model="category_id" name="category_id"
+                        aria-label="Default select example">
+                        <option selected>{{ __('ui.selectCategories') }}</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('category_id')
+                        <span class="small text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                {{-- Image Section --}}
+                <div class="mb-3">
+                    <input wire:model="temporary_images" type="file" name="images" multiple
+                        class="form-control shadow @error('temporary_images.*') is-invalid @enderror"
+                        placeholder="Img" />
+                    @error('temporary_images.*')
+                        <p class="text-danger mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                {{-- Image empty Section --}}
+                @if (!empty($images) || !empty($dbimages))
+                    <div class="section-text mb-3">
+                        <p>Photo preview:</p>
+                        <div class="row border border-4 border-info rounded shadow py-4">
+                            @if ($images != null)
                                 @foreach ($images as $key => $image)
                                     <div class="img-container col my-3">
                                         <div class="img-preview mx-auto shadow rounded"
@@ -65,33 +64,32 @@
                                             wire:click="removeImage({{ $key }})">Cancella</button>
                                     </div>
                                 @endforeach
-                                @endif
-                                @if(isset($dbImages) && !$dbimages->isEmpty())
+                            @endif
+                            @if (isset($dbImages) && !$dbimages->isEmpty())
                                 @foreach ($dbimages as $key => $image)
                                     <div class="img-container col my-3">
                                         <div class="img-preview mx-auto shadow rounded"
-                                            style="background-image:url({{asset('storage/' . $image->path)}});">
+                                            style="background-image:url({{ asset('storage/' . $image->path) }});">
                                         </div>
                                         <button type="button"
                                             class="btn btn-danger shadow d-block text-center mt-2 mx-auto"
                                             wire:click="removeImage({{ $key }})">Cancella</button>
                                     </div>
                                 @endforeach
-                                @endif
-                            </div>
+                            @endif
                         </div>
-                    @endif
-                    {{-- Description Section --}}
-                    <div class="section-split mb-3">
-                        <textarea name="description" wire:model="description" id="description"
-                            placeholder="{{ __('ui.descriptionPlaceholder') }}" class="form form-control">{{ old('decription') }}</textarea>
-                        @error('description')
-                            <span class="small text-danger">{{ $message }}</span>
-                        @enderror
                     </div>
-                    <button class="mt-3 mb-4 large" type="submit">{{ __('ui.button') }}</button>
-                </form>
-            </div>
+                @endif
+                {{-- Description Section --}}
+                <div class="section-text mb-3">
+                    <textarea name="description" wire:model="description" id="description" rows="5"
+                        placeholder="{{ __('ui.descriptionPlaceholder') }}" class="form form-control">{{ old('decription') }}</textarea>
+                    @error('description')
+                        <span class="small text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <button class="mt-3 mb-4 large" type="submit">{{ __('ui.button') }}</button>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/livewire/user-announcements-list.blade.php
+++ resources/views/livewire/user-announcements-list.blade.php
@@ -1,33 +1,32 @@
 <div class="container">
-    <div class="row">
-        I tuoi annunci
+    <div class="row card-container">
+        <h3 class="card-list--title">I tuoi Annunci</h3>
         @foreach ($this->getAnnouncements() as $announcement)
-            <div class="card my-4">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <h6 class="text-body-tertiary mb-0">{{ $announcement->category->name }}</h6>
-                    <span class="text-body-tertiary">Ultima modifica:
-                        {{ $announcement->updated_at->format('d/m/Y') }}</span>
+            <div class="announcement-container">
+                <div class="card-category--container">
+                    <h6 class="category-title">{{ $announcement->category->name }}</h6>
+                    <h6 class="last-update">Ultima modifica:
+                        {{ $announcement->updated_at->format('d/m/Y') }}</h6>
                 </div>
-                <div class="card-body d-flex justify-content-between">
-                    <div class="text-container">
+                <div class="card-body">
+                    <div class="card-img--container">
+                        <img class="img-announcement" src="{{ $announcement->url_image }}"
+                            alt="{{ $announcement->title }}">
+                    </div>
+                    <div class="card-description--container">
                         <h5 class="card-title" wire:model='title'>{{ $announcement->title }}</h5>
-                        <img class="img-fluid" src="{{ $announcement->url_image }}" alt="{{ $announcement->title }}">
-                        <div>
-                            <p class="card-text small">{{ $announcement->description }}</p>
-                        </div>
-
+                        <p class="card-text small">{{ $announcement->description }}</p>
                     </div>
                     <div class="btn-container">
-                        <button class="btn btn-warning text-white bottom"
-                            wire:click="editAnnouncementUser({{ $announcement->id }})">Modifica<i
+                        <button class="btn btn-warning text-white"
+                            wire:click="editAnnouncementUser({{ $announcement->id }})"><i
                                 class="bi bi-pencil-fill"></i></button>
                         <button class="btn btn-danger" wire:click="deleteAnnouncement({{ $announcement->id }})"><i
-                                class="bi bi-trash-fill">Elimina</i></button>
+                                class="bi bi-trash-fill"></i></button>
                     </div>
                 </div>
             </div>
         @endforeach
         {{ $this->getAnnouncementsLinks() }}
-
     </div>
 </div>
