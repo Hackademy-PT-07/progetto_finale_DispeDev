505f2be23f0ce63da088468501f2fb827e7c469d
Thomas Armao <thomas.armao.93@gmail.com>
2023-09-07 19:52:39 +0200

    card annunci corretta

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -49,25 +49,26 @@
         <h2 class="style-title">{{ __('ui.lastAnnouncements') }}</h2>
         <!-- card annunci in primo piano -->
         @forelse ($announcements as $announcement)
-            <div class="card-announcement">
-                <a href="{{ route('annunci.show', $announcement->id) }}">
-                    <div class="card-img-box">
-                        <img class="card-announcement-img"
-                            src="{{ !$announcement->images()->get()->isEmpty()? $announcement->images()->first()->getUrl(400, 300): 'https://picsum.photos/200/300' }}"
-                            alt="{{ $announcement->title }}">
-                    </div>
-                    <div class="card-announcement-text-box">
-                        <a href="{{ route('annunci.filter', ['category' => $announcement->category_id]) }}"
-                            class="category-a">{{ $announcement->category->name }}</a>
-                        <h3>{{ $announcement->title }}</h3>
-                        <span>€{{ $announcement->price }}</span>
-                        <p>{{ $announcement->description }}</p>
-                        <div class="d-flex justify-content-end mt-4">
-                            <p class="last-update">Ultima modifica: {{ $announcement->updated_at }}</p>
+        <a class="a1-card bg-success" href="{{ route('annunci.show', $announcement->id) }}">
+                    <div class="card-announcement ">
+                        <div class="card-img-box">
+                            <img src="{{ !$announcement->images()->get()->isEmpty()? $announcement->images()->first()->getUrl(400, 300): 'https://picsum.photos/200/300' }}" alt="{{ $announcement->title }}">
+                        </div>
+                        <div class="card-announcement-text-box">
+                            <span href="{{ route('annunci.filter', ['category' => $announcement->category_id]) }}"
+                                class="announcement-category">{{$announcement->category->name }}
+                            </span>
+                            <h3 class="announcement-title">{{ $announcement->title }}</h3>
+                            <span class="announcement-price">€{{ $announcement->price }}</span>
+                            <p class="announcement-descr">{{ $announcement->description }}</p>
+                            <div class="d-flex justify-content-end mt-4">
+                                <p class="last-update">{{ __('ui.lastUpdate') }}:
+                                    {{ $announcement->updated_at->format('d/m/Y') }}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </a>
-            </div>
 
 
 
